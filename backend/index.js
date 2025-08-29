aec25bbb4edc9fe42d83c38a6d14cf045b4b3ba7
Surjeet <surjeet@Surjeets-MacBook-Air.local>
2025-08-29 23:04:00 +0530

    remove bugs

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const SECRET_KEY="MeraNameSurjeetHai"
 
 const PORT=process.env.PORT || 3002;
 const DB_URL=process.env.MONGO_URL;
-const cors = require("cors");
+// const cors = require("cors");
 const app=express();
 // app.use(cors());  //cors is s middleware
 // app.use(cors({
@@ -33,13 +33,13 @@ app.use(cors({
 }));
 
 // Explicitly handle preflight
-app.options("*", cors({
-  origin: [
-    "https://zerodha-snowy-three.vercel.app",
-    "https://zerodha-kite-kappa.vercel.app"
-  ],
-  credentials: true
-}));
+// app.options("*", cors({
+//   origin: [
+//     "https://zerodha-snowy-three.vercel.app",
+//     "https://zerodha-kite-kappa.vercel.app"
+//   ],
+//   credentials: true
+// }));
 
 app.use(bodyParser.json())
 app.use(cookieParser());
